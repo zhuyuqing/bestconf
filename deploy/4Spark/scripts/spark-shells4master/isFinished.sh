b12f255b0303ce7e7610bb26d0b836b93dab4a18
liujianxun-ict <liujianxun@ict.ac.cn>
2017-02-17 17:48:44 +0800

    Update isFinished.sh

--- deploy/4Spark/scripts/spark-shells4master/isFinished.sh
+++ deploy/4Spark/scripts/spark-shells4master/isFinished.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 resultFile=/opt/spark/testresults.txt
-#resultFile=./hibench.report
-#while :
-#do
-#for line in `cat  $resultFile`
-
 rm -f ./testresult.txt
 tail -n 2 $resultFile > ./testresult.txt
 
 while read line
 do
-      #echo $line     #这里可根据实际用途变化
-      #target = `echo $line|awk -F ' ' '{print $5}'|tr -d ' '`
-      #duration=`echo $line|awk -F ' ' '{print $6}'|tr -d ' '`      
-      #echo $duration
-
       if [ "$line" == "Run all done!" ];
         then
           echo "ok"
