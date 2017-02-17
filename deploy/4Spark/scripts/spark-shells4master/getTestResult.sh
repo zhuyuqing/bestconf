7b9f27fe185de5e9ab7af281ba9cc5144a0c83ea
liujianxun-ict <liujianxun@ict.ac.cn>
2017-02-17 17:47:30 +0800

    Update getTestResult.sh

--- deploy/4Spark/scripts/spark-shells4master/getTestResult.sh
+++ deploy/4Spark/scripts/spark-shells4master/getTestResult.sh
@@ -1,12 +1,12 @@
 
 resultFile=/opt/HiBench-master/report/hibench.report
-#resultFile=./hibench.report
+
 minduration=0
 i=0
 result=0
 if [  -f "$resultFile" ]; then
 cat $resultFile > /opt/spark/testresult.txt
-#for line in `cat $resultFile`
+
 while read line
 do
         if [ -n "$line" ]; then
@@ -15,7 +15,6 @@ do
                    then
                         ((i++))
                 else 
-                        #echo $duration
                         ((i++))
                         result=`echo "$result + $duration"|bc` 
                         if [ $i == 2 ]; then
