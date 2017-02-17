dc10f078b502bec02746610241c7e94d20a48869
liujianxun-ict <liujianxun@ict.ac.cn>
2017-02-17 17:44:04 +0800

    Update isStart.sh

--- deploy/4Cassandra/scripts/cassandra/isStart.sh
+++ deploy/4Cassandra/scripts/cassandra/isStart.sh
@@ -6,7 +6,6 @@ tail -n 1 $resultFile > /opt/cassandra/startresult.txt
 while read line
 do
       target=`echo ${line:14}`
-      #duration=`echo $line|awk -F ' ' '{print $6}'|tr -d ' '`
       if [ "$target" == "No gossip backlog; proceeding" ]; 
         then 
           echo "ok"
