834333307b5c9ce77b7993547a4a9be3158c815d
liujianxun-ict <liujianxun@ict.ac.cn>
2017-02-17 17:45:21 +0800

    Update terminateSystem.sh

--- deploy/4Cassandra/scripts/cassandra/terminateSystem.sh
+++ deploy/4Cassandra/scripts/cassandra/terminateSystem.sh
@@ -1,3 +1,8 @@
-cd /opt/hadoop-2.6.5/scripts
-./stop.sh
-echo ok
+#!/bin/bash
+path=/opt/cassandra/stopresults.txt
+rm -f /opt/cassandra/stopresults.txt
+touch /opt/cassandra/stopresults.txt
+kill -9 $(ps -ef|grep java |awk '{print $2}') >& $path
+kill -9 $(ps -ef|grep java |awk '{print $2}') >> $path
+kill -9 $(ps -ef|grep java |awk '{print $2}') >> $path
+echo ok >> $path
