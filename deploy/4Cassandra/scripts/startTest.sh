e38e10e700054d58fb5ab9b6e5e752c1d7fc5b7f
liujianxun-ict <liujianxun@ict.ac.cn>
2017-02-17 17:43:12 +0800

    Delete startTest.sh

--- deploy/4Cassandra/scripts/startTest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-path=/opt/cassandra/testresults.txt
-rm -f /opt/cassandra/testresults.txt
-touch /opt/cassandra/testresults.txt
-cd /opt/YCSB
-bin/ycsb run  cassandra2-cql -P  workloads/workloada -p hosts=172.16.48.211,172.16.48.41,172.16.48.209 -p cassandra.username=cassandra -p cassandra.password=cassandra  -p cassandra.maxconnections=20 -p cassandra.coreconnections=6 -threads 16 -s >& $path
-echo ok >> $path
