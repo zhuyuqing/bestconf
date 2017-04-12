4ff44199b50f871b23caa940b1aa00e5b43f2d56
liujianxun-ict <liujianxun@ict.ac.cn>
2017-04-12 10:48:32 +0800

    Delete startTest.sh

--- deploy/4BI/scripts/hadoop-shells4master/startTest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-path=/opt/hadoopshells/testresults.txt
-rm -f /opt/HiBench-master/report/hibench.report
-rm -f /opt/hadoopshells/testresults.txt
-touch /opt/hadoopshells/testresults.txt
-cd /opt/HiBench-master
-bin/run-all.sh >& $path
-
