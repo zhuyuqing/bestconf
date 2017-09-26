52e0e75e80d79166e6af538ee57962ff0698334c
liujianxun-ict <liujianxun@ict.ac.cn>
2017-09-26 11:53:10 +0800

    Update HadoopConfigReadin.java

--- src/BI/cn/ict/zyq/bestConf/cluster/InterfaceImpl/HadoopConfigReadin.java
+++ src/BI/cn/ict/zyq/bestConf/cluster/InterfaceImpl/HadoopConfigReadin.java
@@ -1,7 +1,6 @@
 /**
- * Copyright (c) 2017 Institute of Computing Technology, Chinese Academy of Sciences,  
- * 2017 Institute of Computing Technology, Chinese Academy of Sciences contributors. 
- * All rights reserved.
+ * Copyright (c) 2017 Institute of Computing Technology, Chinese Academy of Sciences, 2017 
+ * Institute of Computing Technology, Chinese Academy of Sciences contributors. All rights reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you
  * may not use this file except in compliance with the License. You
