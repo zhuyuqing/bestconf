0ff086c6d47154ef0ffc756e228b0d9a459f2ea9
liujianxun-ict <liujianxun@ict.ac.cn>
2017-09-26 12:06:58 +0800

    Update SFTPUtil.java

--- src/main/cn/ict/zyq/bestConf/cluster/Utils/SFTPUtil.java
+++ src/main/cn/ict/zyq/bestConf/cluster/Utils/SFTPUtil.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (c) 2017 Institute of Computing Technology, Chinese Academy of Sciences, 2017 
+ * Institute of Computing Technology, Chinese Academy of Sciences contributors. All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
 package cn.ict.zyq.bestConf.cluster.Utils;
 
 import java.io.File;
@@ -34,7 +51,7 @@ public class SFTPUtil {
 			session.setPassword(password);
 			
 			session.setConfig("StrictHostKeyChecking", "no");
-			//30秒连接超�?
+			//time out
 			session.connect(3000);
 		} catch (JSchException e) {
 			e.printStackTrace();
@@ -58,7 +75,6 @@ public class SFTPUtil {
 			} catch (SftpException e) {
 				sftp.mkdir(directory);
 			}
-			//进入目标路径
 			sftp.cd(directory);
 			System.out.println("directory: " + directory);
 			if(file.isFile()){
