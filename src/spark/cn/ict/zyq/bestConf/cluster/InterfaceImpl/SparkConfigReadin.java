7e5902fc7265292244f6e274077b92beef901908
liujianxun-ict <liujianxun@ict.ac.cn>
2017-09-26 12:46:15 +0800

    Update SparkConfigReadin.java

--- src/spark/cn/ict/zyq/bestConf/cluster/InterfaceImpl/SparkConfigReadin.java
+++ src/spark/cn/ict/zyq/bestConf/cluster/InterfaceImpl/SparkConfigReadin.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (c) 2017 Institute of Computing Technology, Chinese Academy of Sciences, 2017 
+ * Institute of Computing Technology, Chinese Academy of Sciences contributors. All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
 package cn.ict.zyq.bestConf.cluster.InterfaceImpl;
 
 import java.io.File;
@@ -69,8 +86,6 @@ public class SparkConfigReadin implements ConfigReadin {
 		HashMap hashmap = null; 
 		File f = new File(filePath);
 		try {
-			 //Properties pps = PropertiesUtil.GetAllProperties(filePath);
-			 //hashmap = Yaml.loadType(new FileInputStream(f.getAbsolutePath()), HashMap.class);
 			Properties pps = PropertiesUtil.GetAllProperties(filePath);
 			 hashmap = new HashMap((Map) pps); 
 		} catch (FileNotFoundException e) {
@@ -98,7 +113,7 @@ public class SparkConfigReadin implements ConfigReadin {
 						 }
 				}
 			}else
-				System.out.println(entUpdated.getKey().toString() + "Doesn't exit in original list£¡");
+				System.out.println(entUpdated.getKey().toString() + "Doesn't exit in original listÂ£Â¡");
 		}
 	}
 	@Override
